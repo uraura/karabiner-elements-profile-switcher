9ec86ac270f71f7bcdc7ae1aad0e6960ef8e6528
uraura <t@urau.la>
2017-07-01 10:50:38 +0900

    😓

--- switcher.lua
+++ switcher.lua
@@ -1,4 +1,4 @@
-local keyboardWatcher = hs.usb.watcher.new(function(params)
+keyboardWatcher = hs.usb.watcher.new(function(params)
     if params["productName"] == "Realforce 87" and params["eventType"] == "added" then
       hs.notify.new({title="Karabiner-Elements Profile Switcher", informativeText=params["productName"].." connected"}):send()
       hs.task.new("/Users/uraura/bin/karabiner-elements-profile-switcher", nil, {"use", "RealForce"}):start()
